9aeb9a1864ee439bd5d9c38d909bf0f64509f571
Khmsik <sthabilekhumalo99@gmail.com>
2024-10-20 10:43:58 +0200

    added API functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,39 @@ function refreshWeather(response) {
   let temparatureElement = document.querySelector("#temperature");
   let temparature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
   temparatureElement.innerHTML = Math.round(temparature);
-  console.log(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windSpeed.innerHTML = response.data.wind.speed;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
