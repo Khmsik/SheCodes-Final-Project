2182cd8e8a5ff9748b45874b901078c4e02ac9e1
Khmsik <sthabilekhumalo99@gmail.com>
2024-10-20 11:21:14 +0200

    added icon data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windSpeed = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
@@ -14,6 +15,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windSpeed.innerHTML = response.data.wind.speed;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
